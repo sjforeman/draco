2498abb106db23c3ecf406f622d70b4bb56da659
Xin Wang <wangxin@cedar1.cedar.computecanada.ca>
2018-11-08 19:28:42 -0800

    legendre initialization

--- draco/analysis/legendre.py
+++ draco/analysis/legendre.py
@@ -12,11 +12,46 @@ from ..core import containers, task, io
 
 
 class LegendreSpectrum(task.SingleTask):
+    """ Calculate the Legendre transform along frequency direction
+
+    Attributes
+    ----------
+
+    """
+
+    def setup(self, telescope):
+        """Set the telescope needed to generate Stokes I.
+
+        Parameters
+        ----------
+        telescope : TransitTelescope
+        """
+        self.telescope = io.get_telescope(telescope)
+
+
+    def process(self, mmodes):
+        """Estimate the Legendre spectrum.
+
+        Parameters
+        ----------
+        mmodes: m-modes transformed data
+
+        Returns
+        -------
+        lspec: LegendreSpectrum
+        """
+
+
+        tel = self.telescope
+
+        ss.redistribute('prod')
+
+        # iterate over all baselines and use
+        for lbi, bi in delay_spec.spectrum[:].enumerate(axis=0):
+            pass
 
 
 
-    def process(self, ss):
-
-
+        return
 
 
